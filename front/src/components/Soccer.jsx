e617f8b78854083ea679981ccb256ad501afecc9
euiho.kim <kim-eh@naver.com>
2023-08-25 23:29:30 +0900

    feat: 팀 순위 관련 정보 추가 및 UI 개선

--- front/src/components/Soccer.jsx
+++ front/src/components/Soccer.jsx
@@ -1,77 +1,135 @@
 import React, { useEffect, useState } from "react";
 import API from "../api/API";
 import ENDPOINT from "../api/ENDPOINT";
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 const Soccer = () => {
   const [soccer, setSoccer] = useState([]);
-  console.table(soccer);
+  // console.table(soccer);
 
   useEffect(() => {
     API(`${ENDPOINT.SOCCER}`, "GET")
       .then((res) => {
-        console.log(res);
         setSoccer(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <>
-      <LayoutContainer>
-        <SoccerUl>
-          {soccer &&
-            soccer.map((soccer) => (
-              <li key={soccer._id}>
-                <InfoUl>
-                  <RankLi>{soccer.rank}</RankLi>
-                  <EmblemLi>
-                    <EmblemImg
-                      src={soccer.emblem}
-                      alt={`${soccer.name}팀의 엠블럼입니다.`}
-                    />
-                  </EmblemLi>
-                  <NameLi>{soccer.name}</NameLi>
-                </InfoUl>
-              </li>
-            ))}
-        </SoccerUl>
-      </LayoutContainer>
-    </>
+    <LayoutContainer>
+      <Table>
+        <thead>
+          <tr>
+            <th id="th">순위</th>
+            <th id="th">팀</th>
+            <th></th>
+            <th id="th">경기</th>
+            <th id="th">승</th>
+            <th id="th">무</th>
+            <th id="th">패</th>
+            <th id="th">득점</th>
+            <th id="th">실점</th>
+            <th id="th">득실차</th>
+            <th id="th">승점</th>
+          </tr>
+        </thead>
+        <tbody>
+          {soccer.map((team) => (
+            <tr key={team._id}>
+              <td>
+                <RankStrong>{team.rank}</RankStrong>
+              </td>
+              <td>
+                <EmblemImg
+                  src={team.emblem}
+                  alt={`${team.name}팀의 엠블럼입니다.`}
+                />
+              </td>
+              <td>
+                <NameStrong>{team.name}</NameStrong>
+              </td>
+              <td>{team.matchesPlayed}</td>
+              <td>{team.wins}</td>
+              <td>{team.draws}</td>
+              <td>{team.losses}</td>
+              <td>{team.goalsFor}</td>
+              <td>{team.goalsAgainst}</td>
+              <td>{team.goalDifference}</td>
+              <td>
+                <PointsStrong>{team.points}</PointsStrong>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </LayoutContainer>
   );
 };
 
 export default Soccer;
 
-const LayoutContainer = styled.div``;
+const LayoutContainer = styled.div`
+  height: 30rem;
+  overflow-y: scroll;
+  padding-right: 1rem;
 
-const SoccerUl = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+  &::-webkit-scrollbar {
+    width: 1rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #ff6f61;
+    border-radius: 2.5rem;
+  }
+  &::-webkit-scrollbar-track {
+    /* background: transparent; */
+  }
 `;
 
-const InfoUl = styled.ul`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-`;
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
 
-const RankLi = styled.li`
-  font-size: var(--fs-sm);
-  color: blueviolet;
-`;
+  th,
+  td {
+    padding: 0.5rem;
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+    vertical-align: middle;
+    font-size: var(--fs-sm);
+    min-width: 4rem;
+    white-space: nowrap;
+  }
 
-const NameLi = styled.li`
-  font-size: var(--fs-sm);
-  font-weight: 700;
-`;
+  th {
+    background-color: #767676;
+    height: 3rem;
+    color: #ffffff;
+    font-weight: 700;
+    padding: initial;
+    font-size: inherit;
+    position: sticky;
+    top: 0;
 
-const EmblemLi = styled.li`
-  color: lightcoral;
+    &#th {
+      width: 4rem;
+      white-space: nowrap;
+    }
+  }
 `;
 
 const EmblemImg = styled.img`
   width: 2rem;
   height: 2rem;
+  margin: 0 auto;
 `;
+
+const Strong = styled.strong`
+  display: block;
+  font-weight: 700;
+`;
+
+const RankStrong = styled(Strong)``;
+const NameStrong = styled(Strong)`
+  text-align: left;
+`;
+const PointsStrong = styled(Strong)``;

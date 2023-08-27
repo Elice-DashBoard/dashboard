3cf0fc5d75b84617b0c3e82adb9f09d1a2bef2cd
euiho.kim <kim-eh@naver.com>
2023-08-27 22:13:32 +0900

    feat: 오늘의 칵테일 추천 기능 추가

--- front/src/components/Cocktail.jsx
+++ front/src/components/Cocktail.jsx
@@ -5,12 +5,11 @@ import { styled } from "styled-components";
 
 const Cocktail = () => {
   const [cocktails, setCocktails] = useState([]);
-  console.table(cocktails);
+  // console.table(cocktails);
 
   useEffect(() => {
     API(`${ENDPOINT.COCKTAILS}`, "GET")
       .then((res) => {
-        console.log(res);
         setCocktails(res.data);
       })
       .catch((err) => console.log(err));
@@ -21,20 +20,38 @@ const Cocktail = () => {
       <LayoutContainer>
         <CocktailUl>
           {cocktails &&
-            cocktails.slice(0, 20).map((cocktail, index) => (
-              <li key={cocktail._id}>
-                <InfoUl>
-                  <CategoryLi>{cocktail.category}</CategoryLi>
-                  <NameLi>{cocktail.name}</NameLi>
-                  <ImageLi>
-                    <CocktailImage
-                      src={cocktail.image}
-                      alt={`${cocktail.name} 이미지입니다.`}
-                    />
-                  </ImageLi>
-                </InfoUl>
-              </li>
-            ))}
+            cocktails
+              .slice(cocktails.length - 1, cocktails.length)
+              .map((cocktail, index) => (
+                <li key={cocktail._id}>
+                  <InfoUl>
+                    <ImageLi>
+                      <CocktailImage
+                        src={cocktail.image}
+                        alt={`${cocktail.name} 이미지입니다.`}
+                      />
+                    </ImageLi>
+                    <CocktailInfoUl>
+                      <H2>Today's Cocktail</H2>
+                      <CategoryAndAlcoholic>
+                        <Li>category : {cocktail.category}</Li>
+                        <Li>
+                          <strong className="highlight">
+                            {cocktail.alcoholic}
+                          </strong>
+                        </Li>
+                      </CategoryAndAlcoholic>
+                      <Li>
+                        name : <strong className="bold">{cocktail.name}</strong>
+                      </Li>
+                      <Li>glass : {cocktail.glass}</Li>
+                      <Li className="ellipsis">
+                        instructions : {cocktail.instructions}
+                      </Li>
+                    </CocktailInfoUl>
+                  </InfoUl>
+                </li>
+              ))}
         </CocktailUl>
       </LayoutContainer>
     </>
@@ -49,28 +66,60 @@ const CocktailUl = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  overflow-x: auto;
 `;
 
 const InfoUl = styled.ul`
   display: flex;
-  align-items: center;
-  gap: 1rem;
-`;
-
-const NameLi = styled.li`
-  font-size: var(--fs-sm);
-  font-weight: 700;
+  gap: 1.5rem;
 `;
 
 const ImageLi = styled.li``;
 
 const CocktailImage = styled.img`
-  width: 2rem;
-  height: 2rem;
+  width: 16rem;
+  height: 16rem;
   border-radius: 2rem;
 `;
 
-const CategoryLi = styled.li`
+const CocktailInfoUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const H2 = styled.h2`
+  font-size: var(--fs-md);
+  font-weight: 700;
+  margin-bottom: 1rem;
+  background: linear-gradient(
+    rgb(255, 111, 97, 0) 0%,
+    rgba(255, 111, 97, 0.3) 100%
+  );
+  width: fit-content;
+`;
+
+const Li = styled.li`
   font-size: var(--fs-sm);
-  color: blueviolet;
+  font-weight: 500;
+  white-space: nowrap;
+  line-height: 1.6;
+
+  & > strong.highlight {
+    color: #ff6f61;
+  }
+
+  & > strong.bold {
+    font-weight: 700;
+    color: black;
+  }
+
+  &.ellipsis {
+    white-space: normal;
+  }
+`;
+
+const CategoryAndAlcoholic = styled.div`
+  display: flex;
+  gap: 1rem;
 `;
